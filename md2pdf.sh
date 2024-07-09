f6f9a2b7f55b5466727a46b7395b49f8d4fdc614
Rui Holdorf <rui.h@me.com>
2024-07-09 11:40:17 -0300

    less verbose

--- md2pdf.sh
+++ md2pdf.sh
@@ -9,7 +9,7 @@ usage() {
     echo "Use: md2pdf.sh -i 'file1.md file2.md ...' -o output_file"
     exit 1
 }
-# verifica os parâmetros nomeados
+# read the options
 while getopts "i:o:" opt; do
     case $opt in
         i)
@@ -23,41 +23,43 @@ while getopts "i:o:" opt; do
             ;;
     esac
 done
-# Verificação se os parâmetros foram fornecidos
+# check if the required arguments were passed
 if [ ${#input_files[@]} -eq 0 ] || [ -z "$output_file" ]; then
     usage
 fi
 
-# Recupera o caminho original do script
+# get the script directory
 script_dir=$(dirname $(realpath $0))
 
-# define o css a ser usado
+# set the css file
 css_file="${script_dir}/estilo.css"
 
-echo "Removing temporary files..."
 # set temp file names
 md_temp="${output_file}.md.tmp"
 html_temp="${output_file}.html.tmp"
-# remove, if there is any
-rm -fv "${md_temp}"
-rm -fv "${html_temp}"
 
-echo "Concatenando para MD único..."
+removeTemp(){
+    # remove, if there is any
+    rm -f "$md_temp"
+    rm -f "$html_temp"
+}
+
+removeTemp
+echo "Concat input files into single MD..."
 for file in "${input_files[@]}"; do
   cat "${file}" >> "${md_temp}"
   echo "\n" >> "${md_temp}"
 done
 
-# cria diretório para imagens, caso não exista
+# create generated images folder, if needed
 if [[ ! -d "$MERMAID_FILTER_LOC" ]]; then
   mkdir -p "$MERMAID_FILTER_LOC"
 fi
 
 echo "From MD to HTML (pandoc)..."
 pandoc -f markdown "${md_temp}" -t html -o "${html_temp}" -F mermaid-filter || { echo "Fail while converting ${base_file}.md.tmp to HTML"; exit 1; }
-
 echo "From HTML to PDF (weasyprint)..."
 weasyprint "${html_temp}" "${output_file}" -s ${css_file} -e utf-8 || { echo "Fail while converting HTML to PDF"; exit 1; }
-
 echo "Done"
+removeTemp
 open "${output_file}"
